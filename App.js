0b4c4fc7204e9169b6c1ee81bc12b873fbc5079c
Gabriel Uccello <gabrielucc@gmail.com>
2021-07-24 16:54:29 -0300

    fonts

--- App.js
+++ App.js
@@ -4,31 +4,58 @@ import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
 import GameOverScreen from "./screens/GameOverScreen";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
+const fetFonts = () => {
+  Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
-  const [guessRounds, setGuessRounds ] = useState(0);
+  const [guessRounds, setGuessRounds] = useState(0);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return <AppLoading
+        startAsync={fetFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    
+  }
 
   const configureNewGameHandler = () => {
     setGuessRounds(0);
     setUserNumber(null);
-  }
+  };
 
   const startGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber);
     setGuessRounds(0);
   };
 
-  const gameOverHandler = numOfRounds => {
-    setGuessRounds(numOfRounds)
-  }
+  const gameOverHandler = (numOfRounds) => {
+    setGuessRounds(numOfRounds);
+  };
 
-  let content = <StartGameScreen onStartGame={startGameHandler}/>;
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
 
   if (userNumber && guessRounds <= 0) {
-    content = <GameScreen userChoise={userNumber} onGameOver={gameOverHandler} />;
+    content = (
+      <GameScreen userChoise={userNumber} onGameOver={gameOverHandler} />
+    );
   } else if (guessRounds > 0) {
-    content = <GameOverScreen roundsNumber={guessRounds} userNumber={userNumber} onRestart={gameOverHandler}/>
+    content = (
+      <GameOverScreen
+        roundsNumber={guessRounds}
+        userNumber={userNumber}
+        onRestart={configureNewGameHandler}
+      />
+    );
   }
 
   return (
